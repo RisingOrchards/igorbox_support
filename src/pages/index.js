37df3807ace3047b1a22bb22387bccadb9709732
Mike Heijmans <parabuzzle@gmail.com>
2024-09-28 07:18:43 -0400

    fix button on mobile

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
           <h1 className="hero__title">{siteConfig.title}</h1>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
         </div>
-        <div className="text--center padding-horiz--md">
+        <div className="text--center padding-horiz--md action-button">
           <Link
             className="button--secondary button--lg margin--sm btn-grad"
             to="/docs/intro"
